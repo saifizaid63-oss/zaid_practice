20e92576af2e195ed7b3251dafa4ba4716771b81
zaid <saifizaid63@gmail.com>
2025-09-24 14:17:54 +0530

    datatype

---
+++ 02_datatype.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as new version 
+
+// alert(3+3) // we are using node.js ,browsere 
+
+console.log(3
+    +3) // code rebality  shoult bhe high 
+
+
+
+console.log("zaid saifi");
+
+let name = " zaid saifi"
+let age = 23
+let isLggedIn = false 
+
+// number => 2 to power 53
+// bighint 
+// string 
+// boolean
+// null
+// undefined
+// symbole
+
+
+
+// object 
+
+console.log(typeof undefined )
