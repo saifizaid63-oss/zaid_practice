4ea6e8cab9d5f9ebc234a462c3d1f73e44d989b7
zaid <saifizaid63@gmail.com>
2025-09-24 17:13:25 +0530

    string

--- 03_convertion.js
+++ 03_convertion.js
@@ -20,6 +20,50 @@ let isLoggedIn = ""
 
 
 let someNumber = String("someNumber")
-console.log(someNumber);
+//console.log(someNumber);
 
-console.log(typeof somenNumber);
+//console.log(typeof somenNumber);
+
+
+// ************* Operation ***************
+
+let value = 3
+let negValue = -value
+ //console.log(negValue)
+ // 
+// console.log(2+2);
+ //console.log(2-2);
+ //console.log(2*2);
+ //console.log(2/2);
+//console.log(2%2);
+
+let ste1 = "hello"
+let str2 = "world"
+
+let str3 = ste1 + " " + str2
+//console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+
+//console.log((3+4)*5%3);
+
+//console.log(+true)
+//console.log(+"");
+
+
+let num1, num2, num3    
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+ 
+
+
+ 
