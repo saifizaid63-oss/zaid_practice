115d31724ce93e6f102e2e6fb2629f9b9cde1e36
zaid <saifizaid63@gmail.com>
2025-09-24 14:44:44 +0530

    convertion

---
+++ 03_convertion.js
@@ -0,0 +1,25 @@
+let score = "zaid saifi"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33"=> 33
+// "33abc"=> NaN 
+//true => 1
+// false=> 0
+
+
+let isLoggedIn = ""
+
+
+//console.log(booleanisLoggedIn );
+
+
+let someNumber = String("someNumber")
+console.log(someNumber);
+
+console.log(typeof somenNumber);
