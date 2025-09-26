e6bbcde016cc60ac25dbe824c3a6509979dd6314
zaid <saifizaid63@gmail.com>
2025-09-26 10:19:51 +0530

    comparision

---
+++ 04_comparision.js
@@ -0,0 +1,16 @@
+//console.log(2>1); // true
+//console.log(2<1); // false
+//console.log(2>=1);      // true
+//console.log(2<=1);      // false
+//console.log(2==1);     // false
+//console.log(2!=1);     // true  
+
+console.log("2" > 1); // true
+console.log("01" == 1); // true
+console.log(true == 1); // true
+console.log(false == 0); // true
+console.log(false == "0"); // true
+
+console.log(false === "0"); // false
+console.log(false === 0); // true
+console.log(false === ""); // false         
