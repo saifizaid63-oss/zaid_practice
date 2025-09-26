51b127ae9698a98e2d44c18524886347a502a8a0
zaid <saifizaid63@gmail.com>
2025-09-26 17:26:49 +0530

    what is string

---
+++ 05_string.js
@@ -0,0 +1,34 @@
+const name = " zaid saifi"
+const repoCount = 50
+
+//console.log(name + repoCount+"value");
+
+console.log(`hello my  name is ${name} and my repoCount is ${repoCount}`);
+
+
+
+
+const gameName = new String ("battle-grount")
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf("g"));
+
+
+
+const newString = gameName.substring(2,6)
+console.log(newString);
+
+
+const anotherString = gameName.slice(-4.8)
+
+
+const newStringOne = "    battle-ground   "
+console.log(newStringOne);
+
+console.log(newStringOne.trim());
+
+const url = "http://zaidhello.html"
+
+console.log(url.replace("zaid","zaidsaifi"));
+
+
