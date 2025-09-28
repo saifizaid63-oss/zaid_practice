84da2554c8ff9aba2f4a25a96dff27ddbaf22c55
zaid <saifizaid63@gmail.com>
2025-09-28 10:30:40 +0530

    math & number

---
+++ 06num_maths.js
@@ -0,0 +1,25 @@
+const score = 400
+//console.log(score);
+
+const balance = new Number (100)
+//console.log(balance);
+
+//console.log(balance.toString().length)
+//console.log(balance.toFixed(2));
+
+const otherNumber = 123.8556
+
+//console.log(otherNumber.toPrecision(3));
+
+const hundreds = 10000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+//++++++++++++++++++++++++++++++MATHS++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(4));
+console.log(Math.round(4.6));
+
+console.log(Math.ceil(4.6));
+console.log(Math.floor(4.6));
+
